df9222872d57869d73c19897bd620fb461cf9dd2
TheSaviourEking <saviour.eking@cyberservices.com>
2025-09-20 21:38:42 +0100

    feat: add server script

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello from a container",
+        service: "hello-node",
+        pod: process.env.POD_NAME || 'unknown',
+        time: new Date().toISOString(),
+    })
+});
+
+app.get("/readyz", (req, res) => res.status(200).send("ready"));
+app.get("/healthz", (req, res) => res.status(200).send("ok"));
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+}).on('error', (err) => {
+    console.error('Server error:', err);
+});
